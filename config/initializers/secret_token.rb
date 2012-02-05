a8a4b98a9e66aba1261f6f187a8d830ad254e801
Daniel <robotright@gmail.com>
2012-02-05 16:34:15 +0800

    intial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ManyMoons::Application.config.secret_token = '680447534c388b03539ae3ec574fb82897836778f34d3b97191590a8bdadc7e69e5915183d618f450a32c6fa7e92ee4f8debd85ca21802e184caa55eb1d5a07e'
