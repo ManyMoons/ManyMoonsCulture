cf04f850656ce21358bb02667c7ccb3950945063
Daniel <robotright@gmail.com>
2012-02-05 17:07:23 +0800

    specify rails generators

--- config/application.rb
+++ config/application.rb
@@ -61,5 +61,11 @@ module ManyMoons
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.generators do |g|
+      g.test_framework :rspec, :views => false, :fixture => true
+      g.fixture_replacement :factory_girl, :dir => 'spec/factories'
+    end
+
   end
 end
