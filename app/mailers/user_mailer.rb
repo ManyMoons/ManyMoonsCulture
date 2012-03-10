ea8ae02aeb72a74283586e6ed46c35bf4dcd8337
Daniel <robotright@gmail.com>
2012-03-10 22:26:30 +0800

    test to see if emails are going through

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,6 +4,7 @@ class UserMailer < ActionMailer::Base
   def contact_us_email(contact, message)
     @contact = contact
     @message = message
-    mail(:to => 'info@manymoonseducation.com', :subject => "Many Moons Education Contact form")
+    #mail(:to => 'info@manymoonseducation.com', :subject => "Many Moons Education Contact form")
+    mail(:to => 'daniel.kiros@gmail.com', :subject => "Many Moons Education Contact form")
   end
 end
